e1f25005c9041115084130b1623321afcd682b19
Yudha <73566152+yudha125@users.noreply.github.com>
2025-06-11 19:06:24 +0700

    Update tambahan.js

--- foreground/tambahan.js
+++ foreground/tambahan.js
@@ -397,7 +397,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     .inner-wrap .panel-blue{
         background: linear-gradient(0deg, rgb(0, 0, 0) 0%, rgb(23, 23, 68) 50%, rgb(81, 105, 167) 100%);
-        max-height:400px;
+        max-height:360px;
         margin-bottom:60px;
     }
     .panel-blue #slider-hasil{
