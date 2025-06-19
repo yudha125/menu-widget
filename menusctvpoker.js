288cc1ffaafa3f4c5b7eda5f3be15100e5c07dff
Yudha <73566152+yudha125@users.noreply.github.com>
2025-06-19 21:43:01 +0700

    Update menusctvpoker.js

--- menusctvpoker.js
+++ menusctvpoker.js
@@ -286,7 +286,7 @@ align-items:center;
                         <img src="https://yudha125.github.io/menu-widget/Image/wa2.webp" alt="Whatsapp SctvPoker" width="40" height="40">
                     </div>
                 </a>
-                <a href="https://pola.rtp-nettvpkr.cyou/" class="link-tombol-widget">
+                <a href="https://info.rtp-sctvpkr.cam/" class="link-tombol-widget">
                     <div class="rtp-button widget-tombol">
                         <img src="https://yudha125.github.io/menu-widget/Image/rtp2.webp" alt="RTP SctvPoker" width="40" height="40">
                     </div>
